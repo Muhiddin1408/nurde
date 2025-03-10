58dca1fad3aa6555180ca7bbde7fe2f37bf55469
Muhiddin <muhiddinturonov14@gmail.com>
2025-03-10 21:48:14 +0500

    commit

--- apps/basic/admin.py
+++ apps/basic/admin.py
@@ -1,9 +1,9 @@
 from django.contrib import admin
-from .models import Specialist, ReadMore, CommentReadMore, Story, OpinioColleague
+from .models import Specialist, ReadMore, CommentReadMore, Story, OpinionColleague
 # Register your models here.
 
 admin.site.register(Specialist)
 admin.site.register(ReadMore)
 admin.site.register(CommentReadMore)
 admin.site.register(Story)
-admin.site.register(OpinioColleague)
+admin.site.register(OpinionColleague)
