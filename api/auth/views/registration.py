f70fe2fb0d57610504411a5eac6046958a13e8ae
Muhiddin <muhiddinturonov14@gmail.com>
2025-04-18 20:55:48 +0500

    commit

--- api/auth/views/registration.py
+++ api/auth/views/registration.py
@@ -80,10 +80,15 @@ def sms_conf(request):
         result = {
             'code': False,
         }
+        print(user)
+        print(sms_code)
+        print(user.sms_code)
         if user and user.sms_code == sms_code:
+            print(sms_code)
             result = {
                 'code': True,
             }
+            return Response(result, status=status.HTTP_200_OK)
         return Response(result, status=status.HTTP_200_OK)
 
     except:
