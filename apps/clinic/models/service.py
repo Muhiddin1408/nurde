5f2925a758034a988cced5fdf072661f38cc497c
Muhiddin <muhiddinturonov14@gmail.com>
2025-04-27 03:17:12 +0500

    commit

--- apps/clinic/models/service.py
+++ apps/clinic/models/service.py
@@ -5,11 +5,15 @@ from apps.utils.models.category import Category
 
 
 class Service(models.Model):
-    image = models.ImageField(upload_to='service', blank=True, null=True)
-    type = models.CharField(max_length=50)
-    name = models.CharField(max_length=255, blank=True, null=True)
+
     clinic = models.ForeignKey(Clinic, on_delete=models.CASCADE)
-    category = models.ManyToManyField(Category, related_name='services')
+    category = models.ForeignKey(Category, on_delete=models.CASCADE, related_name='services', blank=True, null=True)
+    price = models.IntegerField(blank=True, null=True)
+    preparation = models.CharField(max_length=100, blank=True, null=True)
+    time = models.IntegerField(blank=True, null=True)
+    description = models.TextField(blank=True, null=True)
+    created_at = models.DateTimeField(auto_now_add=True, blank=True, null=True)
+    updated_at = models.DateTimeField(auto_now=True, blank=True, null=True)
 
     class Meta:
         verbose_name = 'Услуги'
