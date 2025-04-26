cafbde47199ffbb32f4b435d30c298e33e279d4f
Muhiddin <muhiddinturonov14@gmail.com>
2025-04-26 12:54:58 +0500

    commit

--- api/clinic/views/clinic.py
+++ api/clinic/views/clinic.py
@@ -1,5 +1,6 @@
 from rest_framework import viewsets
 
+from api.basic.views.specialist import SmallPagesPagination
 from api.clinic.serializers.clinic import ClinicSerializers
 from apps.clinic.models import Clinic
 
@@ -7,3 +8,4 @@ from apps.clinic.models import Clinic
 class ClinicViewSet(viewsets.ReadOnlyModelViewSet):
     queryset = Clinic.objects.filter(type='clinic')
     serializer_class = ClinicSerializers
+    pagination_class = SmallPagesPagination
