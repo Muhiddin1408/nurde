7017e69980bd05b774c23e09ecd762025e4ea5a0
Muhiddin <muhiddinturonov14@gmail.com>
2025-04-26 22:24:49 +0500

    commit

--- apps/users/model/address.py
+++ apps/users/model/address.py
@@ -4,15 +4,15 @@ from apps.users.model.patient import Patient
 
 
 class Address(models.Model):
-    name = models.CharField(max_length=100)
+    name = models.CharField(max_length=100, blank=True, null=True)
     street = models.CharField(max_length=100, blank=True, null=True)
     type = models.IntegerField(blank=True, null=True)
     entrance = models.CharField(max_length=125,blank=True, null=True)
     floor = models.CharField(max_length=125,blank=True, null=True)
     number = models.CharField(max_length=125,blank=True, null=True)
     comment = models.TextField(blank=True, null=True)
-    lat = models.FloatField()
-    lng = models.FloatField()
+    lat = models.FloatField(blank=True, null=True)
+    lng = models.FloatField(blank=True, null=True)
     patient = models.ForeignKey(Patient, on_delete=models.CASCADE)
 
     def __str__(self):
