53e7c461fc9e8614963e0bada8fe7338198022ce
Muhiddin <muhiddinturonov14@gmail.com>
2025-04-27 23:43:13 +0500

    commit

--- api/order/urls.py
+++ api/order/urls.py
@@ -1,10 +1,11 @@
 from django.urls import path
 from rest_framework.routers import SimpleRouter
 
-from api.order.views.order import OrderViewSet, ImageViewSet, MyOrderViewSet
+from api.order.views.order import OrderViewSet, ImageViewSet, MyOrderViewSet, DiagnosisViewSet
 
 router = SimpleRouter()
 router.register('my', MyOrderViewSet, basename='my')
+router.register('diagnosis', DiagnosisViewSet, basename='diagnosis')
 
 urlpatterns = [
     path('', OrderViewSet.as_view(), name='order'),
