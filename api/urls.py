efd940de8e4ae8ab4646c3b9606fbd3b7843749a
Muhiddin <muhiddinturonov14@gmail.com>
2025-04-27 22:22:00 +0500

    commit

--- api/urls.py
+++ api/urls.py
@@ -3,7 +3,7 @@ from django.urls import path, include
 urlpatterns = [
     path('auth/', include('api.auth.urls')),
     path('clinic/', include('api.clinic.urls'), name='clinic'),
-    path('laboratory', include('api.labaratory.urls')),
+    path('laboratory/', include('api.labaratory.urls')),
     path('specialist/', include('api.basic.urls'), name='specialist'),
     path('symptom/', include('api.symptom.urls'), name='symptom'),
     path('order/', include('api.order.urls'), name='order'),
