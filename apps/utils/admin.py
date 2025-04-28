db40ee18df4c54eea38c44198ab0dd586413af21
Muhiddin <muhiddinturonov14@gmail.com>
2025-04-28 22:57:09 +0500

    commit

--- apps/utils/admin.py
+++ apps/utils/admin.py
@@ -1,6 +1,8 @@
 from django.contrib import admin
 
 from apps.utils.models import Category
+from apps.utils.models.like import Like
 
 # Register your models here.
 admin.site.register(Category)
+admin.site.register(Like)
