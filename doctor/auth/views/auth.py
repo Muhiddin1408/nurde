2751580e11be0b0cc83a912e54943d2dde449704
Muhiddin <muhiddinturonov14@gmail.com>
2025-04-29 23:39:50 +0500

    commit

--- doctor/auth/views/auth.py
+++ doctor/auth/views/auth.py
@@ -9,10 +9,19 @@ class SpecialistRegister(generics.CreateAPIView):
     serializer_class = SpecialistSerializer
 
 
+class IsActiveUserOrAny(permissions.BasePermission):
+    """
+    Custom permission to allow inactive users to access the view.
+    """
+    def has_permission(self, request, view):
+        # Allow access for both active and inactive users
+        return True
+
+
 class SpecialistUpdate(generics.UpdateAPIView):
     queryset = Specialist.objects.all()
     serializer_class = SpecialistUpdateSerializer
-    permission_classes = [permissions.IsAuthenticated]
+    permission_classes = [permissions.IsAuthenticated, IsActiveUserOrAny]
 
     def get_object(self):
         print(self.request.user)
