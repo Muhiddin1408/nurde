7e3240e93de71886aacd43b5adfc17ae358f0cb7
Muhiddin <muhiddinturonov14@gmail.com>
2025-04-30 00:21:09 +0500

    commit

--- doctor/auth/urls.py
+++ doctor/auth/urls.py
@@ -1,10 +1,11 @@
 from django.urls import path
 
-from doctor.auth.views.auth import SpecialistRegister, SpecialistUpdate, password_conf, CategoryView
+from doctor.auth.views.auth import SpecialistRegister, SpecialistUpdate, password_conf, CategoryView, login
 
 urlpatterns = [
     path('register/', SpecialistRegister.as_view(), name='register'),
     path('password_conf/', password_conf, name='password_conf_doctor'),
     path('update/', SpecialistUpdate.as_view(), name='update'),
     path('category/', CategoryView.as_view(), name='category'),
+    path('login/', login, name='login_doctor'),
 ]
--- doctor/auth/views/auth.py
+++ doctor/auth/views/auth.py
@@ -117,3 +117,30 @@ class CategoryView(generics.ListAPIView):
     serializer_class = CategorySerializer
 
 
+@api_view(['POST'])
+@permission_classes([AllowAny, ])
+def login(request):
+    try:
+        phone = request.data.get('username')
+        password = request.data.get('password')
+        user = User.objects.filter(username=phone, is_staff=True).first()
+        if user and user.check_password(password):
+            user.save()
+            token = RefreshToken.for_user(user)
+            result = {
+                'access': str(token.access_token),
+                'refresh': str(token),
+            }
+
+            return Response(result, status=status.HTTP_200_OK)
+        else:
+            return Response({'msg': "Username or password incorrect"}, status=status.HTTP_404_NOT_FOUND)
+
+    except KeyError:
+        res = {
+            'status': 0,
+            'msg': 'Please set all reqiured fields'
+        }
+        return Response(res)
+
+
