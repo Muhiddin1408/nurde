6fa04ff38304157ecebc591e46472c15587245ff
Muhiddin <muhiddinturonov14@gmail.com>
2025-05-01 22:13:47 +0500

    commit

--- doctor/auth/views/dashboard.py
+++ doctor/auth/views/dashboard.py
@@ -1,5 +1,6 @@
 from rest_framework import generics, permissions
 
+from api.basic.views.specialist import SmallPagesPagination
 from apps.basic.models import Specialist
 from apps.clinic.models import Clinic
 from doctor.auth.serializers.dashboard import DashboardSerializer
@@ -9,6 +10,7 @@ class DashboardView(generics.RetrieveAPIView):
     queryset = Specialist.objects.all()
     serializer_class = DashboardSerializer
     permission_classes = [permissions.IsAuthenticated]
+    pagination_class = SmallPagesPagination
 
     def get_object(self):
         clinic = Specialist.objects.filter(user=self.request.user).last()
