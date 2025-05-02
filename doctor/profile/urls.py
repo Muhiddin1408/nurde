5eaef430465ba5c64847cf1e8b7c71ec33539550
Muhiddin <muhiddinturonov14@gmail.com>
2025-05-02 22:40:21 +0500

    commit

--- doctor/profile/urls.py
+++ doctor/profile/urls.py
@@ -1,7 +1,7 @@
 from django.urls import path
 
 from doctor.profile.views.edu import EducationListCreateView, EducationRetrieveUpdateDestroyView
-from doctor.profile.views.order import OrderView, OrderDetailView, confirm
+from doctor.profile.views.order import OrderView, OrderDetailView, confirm, close
 
 urlpatterns = [
     path('educations/', EducationListCreateView.as_view(), name='education-list-create'),
@@ -9,4 +9,5 @@ urlpatterns = [
     path('order/', OrderView.as_view(), name='order'),
     path('orders/<int:pk>/', OrderDetailView.as_view(), name='order-detail'),
     path('confirm/', confirm),
+    path('close/', close),
 ]
