4600b28361b71f4b7ca82157ce8550bbeec4f2cb
Muhiddin <muhiddinturonov14@gmail.com>
2025-05-03 14:07:04 +0500

    commit

--- doctor/profile/serializers/work.py
+++ doctor/profile/serializers/work.py
@@ -1,5 +1,6 @@
 from rest_framework import serializers
 
+from apps.basic.models import Specialist
 from apps.basic.models.work import WorkImage, Work
 
 
@@ -15,11 +16,16 @@ class WorkSerializer(serializers.ModelSerializer):
 
     class Meta:
         model = Work
-        fields = ['id', 'type', 'name', 'education', 'finish', 'specialist', 'file']
+        fields = ['id', 'type', 'name', 'education', 'finish',  'file']
 
     def create(self, validated_data):
         file_data = validated_data.pop('file', [])
+
+        specialist = self.context['request'].user
+        validated_data['specialist'] = Specialist.objects.get(user=specialist)
         education = super().create(validated_data)
         for file_item in file_data:
-            WorkImage.objects.create(education=education, **file_item)
+            file = WorkImage.objects.get(id=file_item)
+            file.education = education
+            file.save()
         return education
