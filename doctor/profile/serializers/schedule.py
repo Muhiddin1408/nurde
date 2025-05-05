90d7d069d368a43cf92232de60dfd184a76beccd
Muhiddin <muhiddinturonov14@gmail.com>
2025-05-06 00:36:14 +0500

    commit

---
+++ doctor/profile/serializers/schedule.py
@@ -0,0 +1,29 @@
+from rest_framework import serializers
+
+from apps.basic.models import Specialist
+from apps.service.models.service import WorkTime
+
+
+class MyScheduleSerializer(serializers.ModelSerializer):
+    weekday_name = serializers.SerializerMethodField(read_only=True)
+
+    class Meta:
+        model = WorkTime
+        fields = ['id', 'weekday', 'weekday_name', 'date']
+        read_only_fields = ['id', 'weekday_name']
+
+    def get_weekday_name(self, obj):
+        return obj.weekday.name if obj.weekday else None
+
+    def create(self, validated_data):
+        request = self.context.get('request')
+        if request and hasattr(request, 'user'):
+            try:
+                specialist = Specialist.objects.get(user=request.user)
+            except Specialist.DoesNotExist:
+                raise serializers.ValidationError({'user': 'Specialist not found'})
+            validated_data['user'] = specialist
+        else:
+            raise serializers.ValidationError({'user': 'Invalid request context'})
+
+        return super().create(validated_data)
--- doctor/profile/views/schedule.py
+++ doctor/profile/views/schedule.py
@@ -1,7 +1,19 @@
 from rest_framework import generics
+from rest_framework.permissions import IsAuthenticated
 
+from api.basic.serializers.specialist import WorkTimeSerializer
+from apps.basic.models import Specialist
 from apps.service.models.service import WorkTime
+from doctor.profile.serializers.schedule import MyScheduleSerializer
 
 
 class MyScheduleView(generics.ListAPIView):
     queryset = WorkTime.objects.all()
+    serializer_class = MyScheduleSerializer
+    permission_classes = [IsAuthenticated]
+
+    def get_queryset(self):
+        user = self.request.user
+        work_times = WorkTime.objects.filter(user__user=user)
+        return work_times
+
