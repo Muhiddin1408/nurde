ad40295fd4565a0488f1f9b15d20f23bdc3742a3
Muhiddin <muhiddinturonov14@gmail.com>
2025-05-26 10:53:10 +0500

    commit

--- api/clinic/urls.py
+++ api/clinic/urls.py
@@ -3,7 +3,9 @@ from django.urls import path
 from rest_framework.routers import SimpleRouter
 
 from api.clinic.views.clinic import ClinicViewSet
-from api.clinic.views.clinic_detail import CommentView, ClinicServiceDetailView, SpecialistServiceDetailView
+from api.clinic.views.clinic_detail import CommentView, ClinicServiceDetailView, SpecialistServiceDetailView, \
+    create_comment
+
 router = SimpleRouter()
 router.register(r'clinics', ClinicViewSet)
 
@@ -12,4 +14,5 @@ urlpatterns = [
     path('clinics/<int:clinic_id>/comment/', CommentView.as_view(), name='clinic-detail'),
     path('clinics/<int:clinic_id>/services/', ClinicServiceDetailView.as_view(), name='clinic-services'),
     path('clinics/<int:clinic_id>/specialist/', SpecialistServiceDetailView.as_view()),
+    path('comments/create/', create_comment, name='create_comment'),
 ] + router.urls
