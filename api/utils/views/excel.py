f9a3c280642a028836f4f8a9d46497c61e242400
Muhiddin <muhiddinturonov14@gmail.com>
2025-06-13 13:45:05 +0500

    commit

--- api/utils/views/excel.py
+++ api/utils/views/excel.py
@@ -1,20 +1,25 @@
+
+from apps.clinic.models import Symptom
+
+import os
 import pandas as pd
 from rest_framework.decorators import api_view, permission_classes
 from rest_framework.permissions import IsAuthenticated
 from rest_framework.response import Response
 
-from apps.clinic.models import Symptom
 
 
 @api_view(['GET'])
 @permission_classes([IsAuthenticated])
 def symbtom(request):
-    file_path = 'mmm.xlsx'  # Fayl nomini o'zingizga moslashtiring
+    file_path = 'mmm.xlsx'  # Fayl nomini moslashtiring
+    if not os.path.exists(file_path):
+        return Response({"error": "Fayl topilmadi: mmm.xlsx"}, status=400)
+
     df = pd.read_excel(file_path)
 
-    # Ma'lumotlarni ko'rish
     for index, row in df.iterrows():
-        name = f'{row[1]} | {row[2]}'
+        name = f'{row[1]} | {row[2]}'  # Ustun indekslarini tekshiring
         Symptom.objects.create(name=name, type='diagnoses')
 
-    return Response()
+    return Response({"message": "Ma'lumotlar muvaffaqiyatli saqlandi"})
