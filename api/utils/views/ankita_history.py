5f6ac0eb2b7f4251ccec1ded757508249c0b682e
Muhiddin <muhiddinturonov14@gmail.com>
2025-06-15 03:02:29 +0500

    commit

--- api/utils/views/ankita_history.py
+++ api/utils/views/ankita_history.py
@@ -3,6 +3,7 @@ from rest_framework.permissions import IsAuthenticated
 from rest_framework.response import Response
 from rest_framework.views import APIView
 
+from api.basic.views.specialist import SmallPagesPagination
 from api.order.serializers.order import MyOrderSerializers
 from apps.order.models import Order
 
@@ -10,6 +11,11 @@ from apps.order.models import Order
 class AnkitaHistoryViewSet(APIView):
     permission_classes = (IsAuthenticated,)
     def get(self, request, *args, **kwargs):
-        order = Order.objects.filter(ankita_id=kwargs['pk'])
-        serializer = MyOrderSerializers(order, many=True, context={'request': request})
-        return Response(serializer.data)
+        order = Order.objects.filter(ankita_id=kwargs['pk'], status='inactive')
+        paginator = SmallPagesPagination()
+        page = paginator.paginate_queryset(order, request)
+        serializer = MyOrderSerializers(page, many=True, context={"request": request})
+
+        return paginator.get_paginated_response(serializer.data)
+        # serializer = MyOrderSerializers(order, many=True, context={'request': request})
+        # return Response(serializer.data)
