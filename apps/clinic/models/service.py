bc53d66ed1a72bd291f1a18a73768b7df2bfbaaa
Muhiddin <muhiddinturonov14@gmail.com>
2025-06-14 22:08:05 +0500

    commit

--- apps/clinic/models/service.py
+++ apps/clinic/models/service.py
@@ -9,11 +9,12 @@ class Service(models.Model):
     clinic = models.ForeignKey(Clinic, on_delete=models.CASCADE)
     category = models.ForeignKey(Category, on_delete=models.CASCADE, related_name='services', blank=True, null=True)
     price = models.IntegerField(blank=True, null=True)
-    name = models.CharField(max_length=100, blank=True, null=True)
+    preparation = models.CharField(max_length=100, blank=True, null=True)
     time = models.IntegerField(blank=True, null=True)
     description = models.TextField(blank=True, null=True)
     created_at = models.DateTimeField(auto_now_add=True, blank=True, null=True)
     updated_at = models.DateTimeField(auto_now=True, blank=True, null=True)
+    name = models.CharField(max_length=100, blank=True, null=True)
 
     class Meta:
         verbose_name = 'Услуги'
