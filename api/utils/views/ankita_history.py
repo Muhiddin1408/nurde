086a498ce9bb57d164a3fbab68c01a29ac881cd0
Muhiddin <muhiddinturonov14@gmail.com>
2025-06-15 02:57:16 +0500

    commit

--- api/utils/views/ankita_history.py
+++ api/utils/views/ankita_history.py
@@ -1,5 +1,6 @@
 from rest_framework import viewsets
 from rest_framework.permissions import IsAuthenticated
+from rest_framework.response import Response
 from rest_framework.views import APIView
 
 from api.order.serializers.order import MyOrderSerializers
@@ -10,4 +11,4 @@ class AnkitaHistoryViewSet(APIView):
     permission_classes = (IsAuthenticated,)
     def get(self, request, *args, **kwargs):
         order = Order.objects.filter(ankita_id=kwargs['pk'])
-        return MyOrderSerializers(order, many=True).data
+        return Response(MyOrderSerializers(order, many=True).data)
