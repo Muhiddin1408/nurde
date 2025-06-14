6e36a8a74319e1701c5df81fa7b67bd56da578d0
Muhiddin <muhiddinturonov14@gmail.com>
2025-06-14 21:35:35 +0500

    commit

--- doctor/clinic/serializers/service.py
+++ doctor/clinic/serializers/service.py
@@ -4,7 +4,13 @@ from apps.clinic.models import Service
 
 
 class ServiceSerializer(serializers.ModelSerializer):
+    category_name = serializers.SerializerMethodField(read_only=True)
+
     class Meta:
         model = Service
         fields = '__all__'
-        
+
+    def get_category_name(self, obj):
+        if obj.category:
+            return obj.category.name
+        return None
