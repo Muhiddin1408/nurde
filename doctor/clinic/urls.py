9e94915863e434c2d7d1ec9e976d0ea7d39f6de3
Muhiddin <muhiddinturonov14@gmail.com>
2025-06-14 15:25:57 +0500

    commit

--- doctor/clinic/urls.py
+++ doctor/clinic/urls.py
@@ -2,13 +2,14 @@ from django.urls import path
 
 from doctor.clinic.views.change_worker import ChangeWorker, ImageCreateAPIView
 from doctor.clinic.views.clinic_doctor import AdminClinicDoctorView
-from doctor.clinic.views.schedule import WorkTimeCreateClinicView
+from doctor.clinic.views.schedule import WorkTimeCreateClinicView, WorkTimeDoctorClinicView
 from doctor.clinic.views.worker import WorkerView
 
 urlpatterns = [
     path('admin/create/', AdminClinicDoctorView.as_view(), name='admin-create'),
     path('specialist/', WorkerView.as_view(), name='clinic-specialist'),
     path('work/', WorkTimeCreateClinicView.as_view(), name='clinic-specialist'),
+    path('doctor/time/', WorkTimeDoctorClinicView.as_view(), name='clinic-specialist'),
     path('change/status/', ChangeWorker.as_view(), name='change-status'),
     path('image/', ImageCreateAPIView.as_view(), name='image'),
 ]
